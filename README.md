b75bdff6a4783a41aeacef467714f99418644c8e
Alex Aaron <alexfrancisaaron@gmail.com>
2025-05-16 15:40:38 -0500

    added index.js and initial code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # error-practice
+
+What are the errors I want students to spot.
+=> referencde errors for spelling
+=> reference errors associated with dot/bracket notation
+=> reference errors associated with scope? 
+
+### Error 1: Answer the questions for the first error you solve
+* What kind of error is it?
+* Why did this error occur?
+* How did you fix it?
---
+++ index.js
@@ -0,0 +1,41 @@
+
+const blogs = [
+  {
+    id: 0,
+    title: 'Pottery Update',
+    author: 'Stephanie Cooper',
+    text: 'Today, I practiced my sgrafito skills!',
+    category: 'Arts and Leisure',
+    dateCreated: '05/10/2025'
+  },
+  {
+    id: 1,
+    title: 'Videodrome',
+    author: 'Alex Aaron',
+    text: "Rewatched recently. One of Cronenberg's best.",
+    category: 'Film & Television',
+    dateCreated: '05/13/2025'
+  },
+  {
+    id: 2,
+    title: 'Is this the end of the road for the Golden State Warriors?',
+    author: 'Buena Burns',
+    text: "Maybe it's time to trade Steph so he can compete for a championship.",
+    category: 'Sports',
+    dateCreated: '05/15/2025'
+  },
+  {
+    id: 3,
+    title: "Danny Boyle's Shallow Grave",
+    author: 'Stephanie Cooper',
+    text: 'My husband thinks the film is terrific, but the characters were too unlikeable for me.',
+    category: 'Film & Television',
+    dateCreated: '05/16/2025'
+  }
+];
+
+const mostRecentBlog = blogs[length - 1];
+
+if (mostRecentBlog.text.length){
+
+}
